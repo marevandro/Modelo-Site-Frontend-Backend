4d848aafdcb2220d83937a6a029089f426bbe0f6
marevandro <marevandro95@gmail.com>
2023-08-10 19:26:48 -0300

    Resolução problema video

--- js/efects.js
+++ js/efects.js
@@ -29,16 +29,5 @@ $(function () {
   })
 })
 
-(function(d, p) {
-  var a = new XMLHttpRequest(),
-      b = d.body
-  a.open("GET" , p, true);
-  a.send();
-  a.onload = function(){
-    var c = d.createElement("div")
-    c.style.display = "none"
-    c.innerHTML = a.responseText
-    b.insertBefore(c, b.childNodes[0])
-  }
-})(document, "dist/sprint.svg")
+
 
